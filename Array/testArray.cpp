d2a5b2aed7329880c08047ce2e00bdd982809e4d
qasem <2171246jo@s.aou.edu.jo>
2022-10-22 00:26:04 +0300

    code

---
+++ Array/testArray.cpp
@@ -0,0 +1,28 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main() {
+//    int userinput[6];
+//
+//    for (int i = 0; i <6 ; ++i) {
+//        cin>>userinput[i];
+//    }
+//
+//    cout<<"the numbers sre: ";
+//
+//    for (int i = 0; i < 6; ++i) {
+//        cout<<userinput[i]<<" ";
+//    }
+//
+//
+//
+//
+//
+////    int x[] = {19, 10, 8, 17, 9, 15};
+////    for (const int &n: x) {
+////        cout << n << " ";
+////    }
+//
+//    return 0;
+//}
