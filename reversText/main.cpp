ebc13a72007b21087b76ebc58e493a8f39334000
qasem <2171246jo@s.aou.edu.jo>
2022-10-22 15:11:42 +0300

    code

--- reversText/main.cpp
+++ reversText/main.cpp
@@ -18,14 +18,33 @@ void normalPrint(char *s) {
         normalPrint(s + 1);
     }
 }
-// factorial
 
+// factorial
+int factorial(int n) {
+    if (n == 0 || n == 1) return 1;
+
+    return n * factorial(n - 1); //
+}
+
+// factorial
+int factorial_Iter(int n) {
+    int fact = 1;
+    for (int i = n; i > 1; i--) {   //5*1=5 4*5=20 3*20=60 2*60=120
+        fact *= i;
+    }
+    return fact;
+}
 
 int main() {
-    char *s = "Hello Qasem";
-    reverseString(s);
-    cout << "\n*******************************" << endl;
+//    char *s = "Hello Qasem";
+//    reverseString(s);
+//    cout << "\n*******************************" << endl;
+//    normalPrint(s);
+////////////////////////////////////////////////////////////
+//    cout << factorial(4);
+    cout << factorial_Iter(5);
 
-    normalPrint(s);
+////////////////////////////////////////////////////////////
     return 0;
+
 }
